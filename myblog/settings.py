324a3ba579e07b13221605744fd6c4e910683cfa
Valery Maragaño <valery88_7@hotmail.com>
2024-11-07 16:41:13 -0300

    Settings y requirements confirgurada

--- myblog/settings.py
+++ myblog/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 # Cargar variables de entorno desde .env
 load_dotenv()
@@ -67,20 +68,7 @@ WSGI_APPLICATION = "myblog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv(
-            "DB_HOST", "localhost"
-        ),  # Default a localhost si no se especifica
-        "PORT": os.getenv(
-            "DB_PORT", "5432"
-        ),  # Default al puerto 5432 si no se especifica
-    }
-}
+DATABASES = {"default": dj_database_url.config(default=os.getenv("DATABASE_URL"))}
 
 
 # Password validation
